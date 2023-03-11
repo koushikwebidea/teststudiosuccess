30d6f62bb16b9bab84555984025368fa5ed765d1
koushikwebidea <41614233+koushikwebidea@users.noreply.github.com>
2023-03-11 11:56:37 +0530

    Signed-off-by: koushikwebidea <41614233+koushikwebidea@users.noreply.github.com>

---
+++ @core/storage/framework/views/af46886762e0cd0dc5509cdc197506bf2ca0c9ce.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="<?php echo e(asset('assets/backend/css/dropzone.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('assets/backend/css/media-uploader.css')); ?>"><?php /**PATH D:\XAMPP\htdocs\jonny_work\nexelit\@core\resources\views/backend/partials/media-upload/style.blade.php ENDPATH**/ ?>
